d1108e851600faaedffdbb71a8136f2b4b47e6c9
Shivanshi <simis457@gmail.com>
2024-04-18 00:56:41 +0530

    styling

--- item-cart/src/components/MainCart.tsx
+++ item-cart/src/components/MainCart.tsx
@@ -1,53 +1,57 @@
 import React, { useEffect } from "react";
-import {useState} from "react";
+import { useState } from "react";
 import CartItems from "./CartItems.tsx";
-import {ICart, IItem} from "../model/ICart";
+import { ICart, IItem } from "../model/ICart";
 //import CartItems from "./CartItems.tsx";
-
-
 
 const MainCart = () => {
   const [cartItems, setCartItems] = useState<ICart>();
-  
-  const  fetchData  = async() => {
-    const res = await fetch('https://dummyjson.com/carts/user/5');
+
+  const fetchData = async () => {
+    const res = await fetch("https://dummyjson.com/carts/user/5");
     const resJson = await res.json();
     console.log(resJson.carts);
     return resJson.carts;
-  }
-
+  };
 
   useEffect(() => {
-    fetch('https://dummyjson.com/carts/user/5').then(data => data.json()).then(data => setCartItems(data.carts[0]));
+    fetch("https://dummyjson.com/carts/user/5")
+      .then((data) => data.json())
+      .then((data) => setCartItems(data.carts[0]));
     console.log(cartItems);
-  },[]);
+  }, []);
 
-   return(
-    <table>
-     <thead>
-        <tr>
-        <th>Title</th>
-        <th>Price</th>
-        <th>Quantity</th>
-        <th>Total</th>
-        </tr>
+  return (
+    <>
+      <table id="products">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
         </thead>
-      <tbody>
-      <CartItems cartItems={cartItems?.products} />
-      </tbody>
-      <tfoot> 
-            <tr> 
-                <th>TotalQuantity</th>  
-                <td>{cartItems?.totalQuantity}</td> 
-                <th>TotalProducts</th>  
-                 <td>{cartItems?.totalProducts}</td> 
-                <th>TotalPrice</th>  
-                <td>${cartItems?.total}</td> 
-            </tr> 
-        </tfoot>
-      </table>  
+        <tbody>
+          <CartItems cartItems={cartItems?.products} />
+        </tbody>
+      </table>
+      <div className="total">
+        <div>
+        <h4>TotalQuantity:</h4>
+        <p>{cartItems?.totalQuantity}</p>
+        </div>
+        <div>
+        <h4>TotalProducts:</h4>
+        <p>{cartItems?.totalProducts}</p>
+        </div>
+        <div>
+        <h4>TotalPrice:</h4>
+        <p>${cartItems?.total}</p>
+        </div>
+      </div>
+    </>
   );
- 
-}
+};
 
 export default MainCart;
